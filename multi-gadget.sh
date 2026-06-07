05a14c93fd67f4a894e37ad3cc8630d4c62a9853
Sergio Clara <sergio@sergioclara.co.uk>
2026-06-07 21:00:17 +0100

    Keep only gphoto/camera related stuff

--- multi-gadget.sh
+++ multi-gadget.sh
@@ -1,89 +1,81 @@
 #!/bin/bash
-GADGET_PATH=/sys/kernel/config/usb_gadget/pi4
+CONFIGFS="/sys/kernel/config"
+GADGET="$CONFIGFS/usb_gadget/g1"
+FUNCTION="$GADGET/functions/uvc.0"
 
-mkdir $GADGET_PATH
+mkdir -p $FUNCTION
 
+echo 0x1d6b >$GADGET/idVendor
+echo 0x0104 >$GADGET/idProduct
+echo 0x0100 >$GADGET/bcdDevice
+echo 0x0320 >$GADGET/bcdUSB
+echo 0xEF >$GADGET/bDeviceClass
+echo 0x02 >$GADGET/bDeviceSubClass
+echo 0x01 >$GADGET/bDeviceProtocol
 
-echo 0x1d6b > $GADGET_PATH/idVendor
-echo 0x0104 > $GADGET_PATH/idProduct
-echo 0x0100 > $GADGET_PATH/bcdDevice
-echo 0x0200 > $GADGET_PATH/bcdUSB
+mkdir -p $GADGET/strings/0x409
+echo 1000001 >$GADGET/strings/0x409/serialnumber
+echo "Raspberry Pi" >$GADGET/strings/0x409/manufacturer
+echo "Webcam" >$GADGET/strings/0x409/product
 
-echo 0xEF > $GADGET_PATH/bDeviceClass
-echo 0x02 > $GADGET_PATH/bDeviceSubClass
-echo 0x01 > $GADGET_PATH/bDeviceProtocol
+mkdir -p $GADGET/configs/c.1/strings/0x409
+echo 500 >$GADGET/configs/c.1/MaxPower
+echo "UVC" >$GADGET/configs/c.1/strings/0x409/configuration
 
-mkdir $GADGET_PATH/strings/0x409
-echo 100000000d2386db > $GADGET_PATH/strings/0x409/serialnumber
-echo "Samsung" > $GADGET_PATH/strings/0x409/manufacturer
-echo "PI4 USB Device" > $GADGET_PATH/strings/0x409/product
-mkdir $GADGET_PATH/configs/c.1
-mkdir $GADGET_PATH/configs/c.1/strings/0x409
-echo 500 > $GADGET_PATH/configs/c.1/MaxPower
-echo "UVC" > $GADGET_PATH/configs/c.1/strings/0x409/configuration
-
-mkdir $GADGET_PATH/functions/uvc.usb0
-mkdir $GADGET_PATH/functions/acm.usb0
-
-# cat <<EOF $GADGET_PATH/functions/uvc.usb0/control/processing/default/bmControls
+# cat <<EOF $GADGET/functions/uvc0/control/processing/default/bmControls
 # 0
 # 0
 # EOF
 
-mkdir -p $GADGET_PATH/functions/uvc.usb0/control/header/h
-ln -s $GADGET_PATH/functions/uvc.usb0/control/header/h $GADGET_PATH/functions/uvc.usb0/control/class/fs/h
-# ln -s $GADGET_PATH/functions/uvc.usb0/control/header/h $GADGET_PATH/functions/uvc.usb0/control/class/hs/h
-# ln -s $GADGET_PATH/functions/uvc.usb0/control/header/h $GADGET_PATH/functions/uvc.usb0/control/class/ss/h
-
-config_frame () {
+config_frame() {
     FORMAT=$1
     NAME=$2
-	WIDTH=$3
-	HEIGHT=$4
-	
-    framedir=$GADGET_PATH/functions/uvc.usb0/streaming/$FORMAT/$NAME/${HEIGHT}p
+    WIDTH=$3
+    HEIGHT=$4
 
-    mkdir -p $framedir
+    framedir=$FUNCTION/streaming/$FORMAT/$NAME/${HEIGHT}p
 
-    echo $WIDTH > $framedir/wWidth
-    echo $HEIGHT > $framedir/wHeight
-    echo 333333 > $framedir/dwDefaultFrameInterval
-    echo $(($WIDTH * $HEIGHT * 80)) > $framedir/dwMinBitRate
-    echo $(($WIDTH * $HEIGHT * 160)) > $framedir/dwMaxBitRate
-    echo $(($WIDTH * $HEIGHT * 2)) > $framedir/dwMaxVideoFrameBufferSize
-    cat <<EOF > $framedir/dwFrameInterval
+    mkdir -p "$framedir"
+
+    echo "$WIDTH" >"$framedir"/wWidth
+    echo "$HEIGHT" >"$framedir"/wHeight
+    echo 333333 >"$framedir"/dwDefaultFrameInterval
+    echo $(($WIDTH * $HEIGHT * 80)) >"$framedir"/dwMinBitRate
+    echo $(($WIDTH * $HEIGHT * 160)) >"$framedir"/dwMaxBitRate
+    echo $(($WIDTH * $HEIGHT * 2)) >"$framedir"/dwMaxVideoFrameBufferSize
+    cat <<EOF >"$framedir"/dwFrameInterval
 333333
 400000
 666666
 EOF
-
 }
 
-config_frame mjpeg m 640 360
-config_frame mjpeg m 640 480
-config_frame mjpeg m 800 600
-config_frame mjpeg m 1024 768
-config_frame mjpeg m 1280 720
-config_frame mjpeg m 1280 960
-config_frame mjpeg m 1440 1080
-config_frame mjpeg m 1536 864
-config_frame mjpeg m 1600 900
-config_frame mjpeg m 1600 1200
-config_frame mjpeg m 1920 1080
+config_frame mjpeg mjpeg 1056 704
 
+mkdir $FUNCTION/streaming/header/h
 
-mkdir $GADGET_PATH/functions/uvc.usb0/streaming/header/h
-cd $GADGET_PATH/functions/uvc.usb0/streaming/header/h
-ln -s ../../mjpeg/m
-# ln -s ../../uncompressed/u
+# This section links the format descriptors and their associated frames
+# to the header
+cd $FUNCTION/streaming/header/h
+#ln -s ../../uncompressed/yuyv
+ln -s ../../mjpeg/mjpeg
+
+# This section ensures that the header will be transmitted for each
+# speed's set of descriptors. If support for a particular speed is not
+# needed then it can be skipped here.
 cd ../../class/fs
 ln -s ../../header/h
 cd ../../class/hs
 ln -s ../../header/h
-cd ../../../../..
+cd ../../class/ss
+ln -s ../../header/h
+cd ../../../control
 
-ln -s $GADGET_PATH/functions/uvc.usb0 $GADGET_PATH/configs/c.1/uvc.usb0
-ln -s $GADGET_PATH/functions/acm.usb0 $GADGET_PATH/configs/c.1/acm.usb0
+mkdir header/h
+ln -s header/h class/fs
+ln -s header/h class/ss
+
+ln -s $FUNCTION $GADGET/configs/c.1/uvc.usb0
+
 udevadm settle -t 5 || :
-ls /sys/class/udc > $GADGET_PATH/UDC
-
+ls /sys/class/udc >$GADGET/UDC
